8e9010a6e72ae899ef732890357e07b4103f4ba8
Pedro <ppereiramartins@gmail.com>
2016-08-01 22:55:14 +0100

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,12 @@ class UsersController < ApplicationController
   before_action :correct_user,   only: [:edit, :update]
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+#    redirect_to root_url and return unless FILL_IN
   end
 
   def new
