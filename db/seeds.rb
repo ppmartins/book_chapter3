64b29239e4d170f3bb32dc3f667d51ec87b86b86
Pedro <ppereiramartins@gmail.com>
2016-08-12 16:38:48 +0100

    Add user microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,14 @@ User.create!(name:  "Example User",
                activated_at: Time.zone.now)
 end
 
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
+
+
 puts "Rake terminated"
 puts "Result:"
 puts "  - Created #{User.count} new users!"
+puts "  - Created #{Micropost.count} new microposts"
